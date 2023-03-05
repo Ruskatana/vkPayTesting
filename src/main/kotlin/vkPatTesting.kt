e654bf2b3bfbbc98e5027c11d95dab11977d96c7
Katana Ruslan <ruskatanaa@mail.ru>
2023-03-05 19:43:21 +0300

    4 commit

--- src/main/kotlin/vkPatTesting.kt
+++ src/main/kotlin/vkPatTesting.kt
@@ -14,7 +14,7 @@ fun totalComision(typeCard: String, amountThisMonth: Int, amountNow: Int): Int =
         typeCard == "Maestro" && amountNow + amountThisMonth <= 75000 -> 0
         typeCard == "Maestro" && amountNow + amountThisMonth >= 75000 && amountNow < 150000 && amountNow + amountThisMonth < 600000 -> (amountNow * 0.6 / 100 + 20).toInt()
         typeCard == "Visa" && amountNow < 150000 && amountNow + amountThisMonth < 600000 && amountNow * 0.75 / 100 >= 35 -> (amountNow * 0.75 / 100).toInt()
-        typeCard == "Visa" && amountNow < 150000 && amountNow + amountThisMonth < 600000 && amountNow * 0.75 / 100 <= 35 -> 35
+        typeCard == "Visa" && amountNow < 150000 && amountNow + amountThisMonth < 600000 && amountNow * 0.75 / 100 < 35 -> 35
         typeCard == "Мир" && amountNow < 150000 && amountNow + amountThisMonth < 600000 && amountNow * 0.75 / 100 >= 35 -> (amountNow * 0.75 / 100).toInt()
         typeCard == "Мир" && amountNow < 150000 && amountNow + amountThisMonth < 600000 && amountNow * 0.75 / 100 <= 35 -> 35
         typeCard == "VK PAY" && amountNow < 15000 && amountNow + amountThisMonth < 40000 -> 0
--- src/test/kotlin/VkPatTestingKtTest.kt
+++ src/test/kotlin/VkPatTestingKtTest.kt
@@ -5,16 +5,25 @@ import org.junit.Assert.*
 class VkPatTestingKtTest {
 
     @Test
-    fun totalComision() {
+    fun totalComisionVisa1() {
         var typeCard = "Visa"
         var amountThisMonth = 10
         var amountNow= 200
         var result = totalComision(typeCard, amountThisMonth, amountNow)
-
         assertEquals(35, result)
     }
     @Test
-    fun totalComision2() {
+    fun totalComisionVisa2() {
+        var typeCard = "Visa"
+        var amountThisMonth = 500000
+        var amountNow= 30000
+        var result = totalComision(typeCard, amountThisMonth, amountNow)
+        assertEquals(225, result)
+    }
+
+
+    @Test
+    fun totalComisionMastercard1() {
         var typeCard = "Mastercard"
         var amountThisMonth = 700
         var amountNow= 500
@@ -23,7 +32,17 @@ class VkPatTestingKtTest {
         assertEquals(0, result)
     }
     @Test
-    fun totalComision3() {
+    fun totalComisionMastercard2() {
+        var typeCard = "Mastercard"
+        var amountThisMonth = 50000
+        var amountNow= 26000
+        var result = totalComision(typeCard, amountThisMonth, amountNow )
+
+        assertEquals(176, result)
+    }
+
+    @Test
+    fun totalComisionMir1() {
         var typeCard = "Мир"
         var amountThisMonth = 50
         var amountNow= 460
@@ -32,12 +51,48 @@ class VkPatTestingKtTest {
         assertEquals(35, result)
     }
     @Test
-    fun totalComision4() {
+    fun totalComisionMir2() {
+        var typeCard = "Мир"
+        var amountThisMonth = 100000
+        var amountNow= 30000
+        var result = totalComision(typeCard, amountThisMonth, amountNow )
+
+        assertEquals(225, result)
+    }
+    @Test
+    fun totalComisionVkPay1() {
         var typeCard = "VK PAY"
         var amountThisMonth = 799
         var amountNow= 500
         var result = totalComision(typeCard, amountThisMonth, amountNow )
 
         assertEquals(0, result)
+    }
+    @Test
+    fun totalComisionMaestro1() {
+        var typeCard = "Maestro"
+        var amountThisMonth = 799
+        var amountNow= 500
+        var result = totalComision(typeCard, amountThisMonth, amountNow )
+
+        assertEquals(0, result)
+    }
+    @Test
+    fun totalComisionMaestro2() {
+        var typeCard = "Maestro"
+        var amountThisMonth = 60000
+        var amountNow= 100000
+        var result = totalComision(typeCard, amountThisMonth, amountNow )
+
+        assertEquals(620, result)
+    }
+    @Test
+    fun totalComisionMistake() {
+        var typeCard = "-1"
+        var amountThisMonth = 60000
+        var amountNow= 100000
+        var result = totalComision(typeCard, amountThisMonth, amountNow )
+
+        assertEquals(-1, result)
     }
 }
